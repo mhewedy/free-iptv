ab79012368944032be9ba3e36427a4d72c015a4a
mhewedy <mhewedy@gmail.com>
2019-10-17 15:58:36 +0300

    setting up retrials

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
@@ -13,14 +14,38 @@ func main() {
 		os.Exit(-1)
 	}
 
-	iptvLink, err := GetIPTVLink()
-	fmt.Println(iptvLink)
-	if err != nil {
-		log.Fatal(err)
+	var err error
+	trial, max := 1, 20
+
+	for {
+		log.Println(">>>>> starting trial #", trial, "<<<<<")
+		err = Do(os.Args[1])
+
+		if err == nil || trial >= max {
+			break
+		}
+		trial += 1
+		duration := time.Duration(1000*trial) * time.Millisecond
+		log.Println("sleeping", duration)
+		time.Sleep(duration)
 	}
 
-	err = UpdateSmartTVApp(iptvLink, os.Args[1])
-	if err != nil {
-		log.Fatal(err)
+	if err == nil {
+		fmt.Println("succeed at trail #", trial)
 	}
+}
+
+func Do(macAddress string) error {
+	iptvLink, err := GetIPTVLink()
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	fmt.Println(iptvLink)
+	err = UpdateSmartTVApp(iptvLink, macAddress)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
 }
